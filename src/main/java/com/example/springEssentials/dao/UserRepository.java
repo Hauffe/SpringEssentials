18a38a1d53c88d3efa91f7ea978b0915ea375a27
Alexandre Hauffe <alexandre.hauffe@avenuecode.com>
2021-06-07 13:12:53 -0300

    Some corrections

--- src/main/java/com/example/springEssentials/dao/UserRepository.java
+++ src/main/java/com/example/springEssentials/dao/UserRepository.java
@@ -3,8 +3,7 @@ package com.example.springEssentials.dao;
 import com.example.springEssentials.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
